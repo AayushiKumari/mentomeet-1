77e23b9ab4e1e1806af0976d4a883b68026a4a4e
g-abhishek <ag528927@gmail.com>
2020-08-29 13:20:34 +0530

    login page done and some minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Index from './components/index.js'
 import Question from './components/Question/Question.js'
 import Answer from './components/Answer/Answer'
+import Login from './components/Login/Login'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
                 <Route path="/index" render={props => <Index {...props} /> } />
                 <Route path="/qna" render={props => <Question {...props} /> } />
                 <Route path="/answer" render={props => <Answer {...props} /> } />
+                <Route exact path="/login" render={props => <Login {...props} /> } />
                 {/* <Route path="/login" render={props => <Login {...props} /> } />
                 <Route path="/student" render={props => <Student {...props} /> } /> */}
 
